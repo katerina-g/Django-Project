aa341058aa5c7a77ca695828baa9008b318e8443
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-04-04 11:23:48 +0300

    Added like and comment models

--- recipes_app/recipes_app/main/models.py
+++ recipes_app/recipes_app/main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from recipes_app.recipes.models import Recipe
+
+UserModel = get_user_model()
+
+
+class Comment(models.Model):
+    text_comment = models.TextField()
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
+
+
+class Like(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
