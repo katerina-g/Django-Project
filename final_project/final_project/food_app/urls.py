bb58823b06f5b91d40db641a10ff4756156a9670
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-04-04 09:37:41 +0300

    remove

--- final_project/final_project/food_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from final_project.food_app.views import HomeView, CommentRecipeView
-
-urlpatterns = (
-    path('', HomeView.as_view(), name='home'),
-    path('comment/<int:pk>', CommentRecipeView.as_view(), name='comment'),
-    # path('home/', HomeFirstView.as_view(), name='home'),
-)
--- final_project/final_project/recipes/validators.py
+++
@@ -1,9 +0,0 @@
-from re import compile
-from django.core.exceptions import ValidationError
-
-pattern = compile(r"^((\w+)(,\s\w+))+$")
-
-
-def validate_ingredients(input_string):
-    if pattern.match(input_string) is None:
-        raise ValidationError('The ingredients should be separated by ", "!')
