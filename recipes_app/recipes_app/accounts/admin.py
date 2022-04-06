1a4b09e38999ab8af58687b501507f1531ea0ca5
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-04-06 10:54:58 +0300

    Updated admin.py

--- recipes_app/recipes_app/accounts/admin.py
+++ recipes_app/recipes_app/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes_app.accounts.models import Profile, RecipesUser
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+@admin.register(RecipesUser)
+class RecipesUserAdmin(admin.ModelAdmin):
+    pass
