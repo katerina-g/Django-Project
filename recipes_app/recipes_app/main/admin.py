1d076da592ddd6f3be2de3bff5bfb91922fe0886
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-04-06 12:42:21 +0300

    Added admin.py

--- recipes_app/recipes_app/main/admin.py
+++ recipes_app/recipes_app/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from recipes_app.main.models import Comment, Like
 
+
+@admin.register(Comment)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'user')
--- recipes_app/recipes_app/recipes/admin.py
+++ recipes_app/recipes_app/recipes/admin.py
@@ -7,4 +7,3 @@ from recipes_app.recipes.models import Recipe
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('recipe_name', 'ingredients', 'recipe_picture', 'how_to_make','category', 'created_on', 'created_by')
-    
