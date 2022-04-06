700b9017bb9f8d6fdaf550d6ad626dcb3f6632c0
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-04-06 11:11:00 +0300

    Added admin.py

--- recipes_app/recipes_app/recipes/admin.py
+++ recipes_app/recipes_app/recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from recipes_app.recipes.models import Recipe
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipe_name', 'ingredients', 'recipe_picture', 'how_to_make','category', 'created_on', 'created_by')
+    
