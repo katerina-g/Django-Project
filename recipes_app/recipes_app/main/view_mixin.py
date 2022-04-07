f28357b63e210bb5f0e0a398347f582649342f3e
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-04-07 17:55:10 +0300

    fixed view_mixin

--- recipes_app/recipes_app/main/view_mixin.py
+++ recipes_app/recipes_app/main/view_mixin.py
@@ -6,6 +6,6 @@ from recipes_app.accounts.models import Profile
 class RedirectToCreateProfile:
     def dispatch(self, request, *args, **kwargs):
         profile = Profile.objects.filter(user_id=request.user.id)
-        if not profile:
+        if not profile and not request.user.is_superuser:
             return redirect('create profile')
         return super().dispatch(request, *args, **kwargs)
